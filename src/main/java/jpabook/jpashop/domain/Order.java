beab4b9b093ce4a10810b46440331c4979910202
rojae <Jae.Thon96@gmail.com>
2021-07-13 23:39:36 +0900

    init commit (Create Entity Table)

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    // ONE TO ONE의 경우 접근이 더 많은 컬럼에 주인등록
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate;        // 주문시간
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;             // 주문상태 [ORDER, CANCEL]
+
+}
