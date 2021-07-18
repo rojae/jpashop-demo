9073c7aba659bb17a2c536ee98affc95c6e5f5e8
rojae <Jae.Thon96@gmail.com>
2021-07-18 18:42:39 +0900

    ADD) Entity 추가 및 Fetch LAZY로 통합 - Address의 protected는 jpa 스펙에 의해서 default 생성자가 필요하여 추가. (사용하지 않기 때문에 not public)

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -17,15 +17,15 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
     // ONE TO ONE의 경우 접근이 더 많은 컬럼에 주인등록
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -33,5 +33,7 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;             // 주문상태 [ORDER, CANCEL]
+
+
 
 }
--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -20,5 +22,8 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany
+    private List<Category> categories = new ArrayList<>();
 
 }
